071923a21f38b932810a2c20f34bad5a77e92fda
Goaty1208 <goaty1208@gmail.com>
2024-09-16 18:47:20 +0200

    Assets Asset pusher AddAsset

--- src/assets.cpp
+++ src/assets.cpp
@@ -1,2 +1,18 @@
+/*******************************************************************************************
+*
+*   assets.cpp   
+*
+*******************************************************************************************/
+
 #include "assets.hpp"
 
+LonginusAssets::TextureAsset test = {"resources/test.png"};
+
+/*Adds Texture2D to texture list, increases texture counter.*/
+bool LonginusAssets::AddAsset(const char* fileName) {
+
+    LonginusAssets::CurrentGameTextures.push_back({fileName, LonginusAssets::TextureAssetCounter, 0});
+    LonginusAssets::TextureAssetCounter++;
+    
+    return true;
+}
--- src/assets.hpp
+++ src/assets.hpp
@@ -1,10 +1,30 @@
+/*******************************************************************************************
+*
+*   assets.hpp   
+*
+*******************************************************************************************/
+
 #ifndef ASSETS_HPP
 #define ASSETS_HPP
 
 #include "raylib.h"
+#include <vector>
 
 namespace LonginusAssets {
     
+    /*Texture asset. Will the InternalID actually have a purpose? I don't know, probably, I'd guess.*/
+    typedef struct {
+        const char* Name;
+        const int InternalID;
+        Texture2D Texture;
+    } TextureAsset;
+    
+    int TextureAssetCounter = 0;
+    std::vector<LonginusAssets::TextureAsset> CurrentGameTextures;
+
+    bool AddAsset(const char* fileName);
+    void LoadAllAssets();
+
 } // namespace LonginusAssets
 
 
