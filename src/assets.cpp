2e9b2d5c2620f8a99ebf9066f414d67af5e86c55
Goaty1208 <goaty1208@gmail.com>
2024-09-16 19:08:29 +0200

    Asset Map.

--- src/assets.cpp
+++ src/assets.cpp
@@ -8,7 +8,13 @@
 
 LonginusAssets::TextureAsset test = {"resources/test.png"};
 
-/*Adds Texture2D to texture list, increases texture counter.*/
+/*Adds Texture2D to texture map, increases texture counter.
+  Unlike a shitty ol' vector, the amazing map allows us to use a cool ass ID which I'd guess is better as assets load in a better way.
+  I'd assume, at least.
+  
+  But hey, it is quite efficient
+*/
+
 bool LonginusAssets::AddAsset(LonginusAssets::TextureAsset asset) {
 
     LonginusAssets::CurrentGameTextures[asset] = LonginusAssets::TextureAssetCounter;
