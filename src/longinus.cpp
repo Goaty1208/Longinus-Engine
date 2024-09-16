f131d024f6048bcf2e0694a16897bd6ee9b86984
Goaty1208 <goaty1208@gmail.com>
2024-09-16 18:34:10 +0200

    Assets created

--- src/longinus.cpp
+++ src/longinus.cpp
@@ -1,11 +1,9 @@
 /*******************************************************************************************
 *
-*   raylib game template
-*
 *   Longinus Engine
 *   3D Quake-like engine
 *
-*   This game has been created using raylib (www.raylib.com)
+*   This engine has been created using raylib (www.raylib.com)
 *   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
 *
 *   Copyright (c) 2021 Ramon Santamaria (@raysan5)
